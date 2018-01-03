ec4168e1a815d345fdc32cedd926fc0913c5dfae
守屋千春 <mry116k@hotmail.com>
2018-01-03 12:56:45 +0900

    postメソッドを受け付ける

---
+++ post-test.js
@@ -0,0 +1,33 @@
+//281P
+//Postで値を受け取る
+
+//Expressのモジュールを取り込んで作成
+const express = require('express')
+const app = express()
+
+//落ち受ける、サーバーを起動
+app.listen(3000, ()=>{
+  console.log('起動しました')
+})
+
+// GETメソッドならWebフォームを表示
+app.get('/', (req, res, next) => {
+    res.send('<form method="POST">' +
+    '<textarea name="memo">テスト</textarea>' +
+    '<br><input type="submit" value="送信">' +
+    '</form>')
+})
+
+// POSTメソッドでを受け付ける
+app.post('/', (req, res) => {
+  res.send('POSTされました')
+})
+
+//サイコロのランダムを算出する関数
+function dice(n) {
+  return Math.floor(Math.random() * n) +1
+}
+
+// $>node dice.js
+//アクセス
+//http://localhost:3000
